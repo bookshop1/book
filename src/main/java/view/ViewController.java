615ed667331d692bfde67a8a83681d22f9adb14a
janghj925 <episode3333@naver.com>
2025-07-07 22:30:40 +0900

    view

---
+++ src/main/java/view/ViewController.java
@@ -0,0 +1,30 @@
+package view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("view")
+public class ViewController {
+	
+	@Autowired
+	ViewService service;
+	
+	
+	
+	@GetMapping("/detail")
+	public String detail(@RequestParam("id") int id, Model model) {
+		View view = service.getDetail(id);
+		model.addAttribute("view", view);
+		
+		return "detail";
+	}
+	
+	
+
+}
