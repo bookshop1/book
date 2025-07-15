7d28d019b6f4dd6281d20b2a66bee4ef2eee086e
minchang <min02279791@gmail.com>
2025-07-15 16:49:33 +0900

    min

--- src/main/java/login/LoginService.java
+++ src/main/java/login/LoginService.java
@@ -3,13 +3,15 @@ package login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("loginService") //setting.xml에 bean설정 필요없는 대신 (context-scan) 해줘야함
+import join.UserVO;
+
+@Service
 public class LoginService {
-	@Autowired
-	LoginMapper mapper;
-	public int login(String id, String password) {
-        // 실제 구현은 DB에서 조회 후 일치 여부 판단
-        // 예) loginMapper.findByIdAndPassword(id, password) != null
-        return mapper.login(id, password);
+
+    @Autowired
+    private LoginMapper mapper;
+
+    public UserVO getUser(String id, String password) {
+        return mapper.getUser(id, password);
     }
 }
