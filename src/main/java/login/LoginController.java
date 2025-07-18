fa7adcbe75c6d9f8e82714db6350945217c55ceb
Resok1306 <jms135756@gmail.com>
2025-07-18 14:37:33 +0900

    login rollback

--- src/main/java/login/LoginController.java
+++ src/main/java/login/LoginController.java
@@ -1,9 +1,16 @@
 package login;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import join.UserVO;
 
 @Controller
 @RequestMapping("/login")
@@ -12,11 +19,45 @@ public class LoginController {
     @Autowired
     private LoginService service;
 
-   
+    // 로그인 폼 화면
     @GetMapping("/loginform")
     public String loginForm() {
         return "loginform"; // /WEB-INF/views/loginform.jsp
     }
+    
 
+    @PostMapping
+    public String loginProcess(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            Model model,
+            HttpSession session) {
+
+        UserVO loginUser = service.getUser(username, password);
+
+        if (loginUser != null) {
+            // 로그인 성공 시 세션에 사용자 정보 저장
+            session.setAttribute("loginUser", loginUser);
+            session.setAttribute("userId", loginUser.getU_id());
+            
+        	if("ROLE_ADMIN".equals(loginUser.getRole())) {
+
+            return "redirect:/admin/main"; // 로그인 성공 시 이동할 페이지
+        	}else
+        		return "redirect:/main";
+        } else {
+            model.addAttribute("errorMsg", "아이디 또는 비밀번호가 잘못되었습니다.");
+            return "loginform"; // 로그인 실패 시 재로그인 폼
+        }
+    }
+    
+
+ 
+    
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate(); // 모든 세션 정보 제거
+        return "redirect:/login/loginform";
+    }
 
 }
--- src/main/java/login/LoginService.java
+++ src/main/java/login/LoginService.java
@@ -1,35 +1,17 @@
 package login;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service("loginService") //setting.xml에 bean설정 필요없는 대신 (context-scan) 해줘야함
-public class LoginService implements UserDetailsService {
-	@Autowired
-	LoginMapper mapper;
+import join.UserVO;
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		MemberVO member = mapper.findById(username);
-		
-		if(username == null) {
-			 throw new UsernameNotFoundException("사용자 없음");
-		}
-		
-		List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(member.getRole()));
+@Service
+public class LoginService {
 
-        return new User(member.getId(), member.getPassword(), authorities);
-	}	
+    @Autowired
+    private LoginMapper mapper;
+
+    public UserVO getUser(String id, String password) {
+        return mapper.getUser(id, password);
+    }
 }
