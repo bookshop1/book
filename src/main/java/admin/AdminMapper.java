8c12278b36dd90b209fc892c4649c7043a9dc00b
minchkm <min02279791@gmail.com>
2025-08-31 12:38:45 +0900

    Update AdminMapper.java

--- src/main/java/admin/AdminMapper.java
+++ src/main/java/admin/AdminMapper.java
@@ -5,11 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-
-import main.Book;
 
 
 @Mapper
@@ -21,7 +18,7 @@ public interface AdminMapper {
 	@Select("select * from book where b_id = #{b_id}")
 	public AdminBook findById(int b_id);
 
-	@Insert("INSERT INTO book (b_id, num, pic, title, author, price, info, category) VALUES (book_id_seq.NEXTVAL, 0, #{pic}, #{title}, #{author}, #{price}, #{info}, #{category})")
+	@Insert("INSERT INTO book (b_id, num, pic, title, author, price, info) VALUES (book_id_seq.NEXTVAL, #{num}, #{pic}, #{title}, #{author}, #{price}, #{info})")
 	public int insertbook(AdminBook book);
 
 	@Delete("DELETE FROM book WHERE b_id = #{b_id}")
@@ -29,19 +26,11 @@ public interface AdminMapper {
 
 	@Update("UPDATE book SET " +
             "num = #{num}, " +
-            "pic = 0, " +
+            "pic = #{pic}, " +
             "title = #{title}, " +
             "author = #{author}, " +
             "price = #{price}, " +
-            "info = #{info}, " +
-            "category = #{category} " +
+            "info = #{info} " +
             "WHERE b_id = #{b_id}")
 	public void updateBook(AdminBook book);
-	
-	@Select("select * from book where title like '%' || #{keyword} || '%' "
-			+ "or author like '%' || #{keyword} || '%'")
-	public List<AdminBook> search(String keyword);
-	
-	@Select("SELECT * FROM book WHERE category = #{category}")
-    List<AdminBook> findByCategory(@Param("category") String category);
 }
